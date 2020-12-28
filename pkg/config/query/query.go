e0a258c30d8a9d7e096d3c0b30df274555f1b23e
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-12-28 12:10:51 -0800

    Add query package for xApps

---
+++ pkg/config/query/query.go
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package query
+
+import (
+	"github.com/openconfig/ygot/ygot"
+	"github.com/tidwall/gjson"
+)
+
+// Query
+type Query struct {
+	Config *ygot.ValidatedGoStruct
+}
+
+// GetJsonConfig emits RFC7951 Json format of configuration
+func (q *Query) GetJsonConfig() (string, error) {
+	JsonConfig, err := ygot.EmitJSON(*q.Config, &ygot.EmitJSONConfig{
+		Format: ygot.RFC7951,
+		Indent: "  ",
+		RFC7951Config: &ygot.RFC7951JSONConfig{
+			AppendModuleName: true,
+		},
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return JsonConfig, nil
+}
+
+// GetJsonValue
+func (q *Query) GetJsonValue(key string) (interface{}, error) {
+	jsonConfig, err := q.GetJsonConfig()
+	if err != nil {
+		return nil, err
+	}
+	value := gjson.Get(jsonConfig, key)
+	return value, nil
+
+}
