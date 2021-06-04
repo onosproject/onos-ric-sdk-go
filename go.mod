a62bb7a6c467d900fc1a9249fa2b512564f5c932
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2021-06-04 13:36:57 -0700

    Improve topo sdk (#70)
    * implement initial version of topo sdk
    
    * go mod tidy
    
    * revert some go.mod files
    
    * update deps
    
    * update go.mod
    
    * remove unused fields
    
    * use options for list and watch methods
    
    * Use options pattern
    
    * add update method
    
    * Use options for new client
    
    * rebase and go mod tidy
    
    * clean up
    
    * cleanup
    
    * clean up
    
    * rename internal client
    
    * Address comments
    
    * Use one layer of abstraction for topo sdk
    
    * update go.mod
    
    * Add service option helpers
    
    * Address comments

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/onosproject/onos-api/go v0.7.20
+	github.com/onosproject/onos-api/go v0.7.50
 	github.com/onosproject/onos-lib-go v0.7.0
 	github.com/openconfig/gnmi v0.0.0-20200617225440-d2b4e6a45802
 	github.com/pelletier/go-toml v1.8.1 // indirect
@@ -30,11 +30,9 @@ require (
 	github.com/spf13/viper v1.7.1 // indirect
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
-	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
 	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/tools v0.0.0-20201116002733-ac45abd4c88c // indirect
 	google.golang.org/appengine v1.6.5 // indirect
-	google.golang.org/genproto v0.0.0-20201113130914-ce600e9a6f9e // indirect
 	google.golang.org/grpc v1.33.2
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
--- pkg/topo/topo.go
+++ pkg/topo/topo.go
@@ -2,70 +2,189 @@
 //
 // SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
 
-// Package topo contains facilities for RIC applications to monitor topology for changes
 package topo
 
 import (
 	"context"
-	topoapi "github.com/onosproject/onos-api/go/onos/topo"
-	"github.com/onosproject/onos-lib-go/pkg/errors"
-	"github.com/onosproject/onos-lib-go/pkg/logging"
-	"google.golang.org/grpc/status"
 	"io"
+
+	"github.com/onosproject/onos-lib-go/pkg/errors"
+	"google.golang.org/grpc/status"
+
+	"github.com/onosproject/onos-ric-sdk-go/pkg/utils/creds"
+	"google.golang.org/grpc/credentials"
+
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
+
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+
+	"time"
+
+	"github.com/onosproject/onos-ric-sdk-go/pkg/topo/connection"
+
+	"github.com/onosproject/onos-lib-go/pkg/southbound"
+	"google.golang.org/grpc"
 )
 
-var log = logging.GetLogger("topo", "watch")
+var log = logging.GetLogger("topo")
 
-// Watch provides a simple facility for the application to watch for changes in the topology.
-func (c *topoClient) Watch(ctx context.Context, ch chan<- topoapi.Event, filter ...FilterOption) error {
-	// Issue a watch request with replay and passing through any service-side filter criteria, i.e.
-	// those supported by the topo.Watch method (TBD)
-	stream, err := c.client.Watch(ctx, &topoapi.WatchRequest{})
-	if err != nil {
-		log.Error("Unable to issue topology watch request", err)
-		stat, ok := status.FromError(err)
-		if ok {
-			return errors.FromStatus(stat)
-		}
-		return err
+// Client is a topo SDK client
+type Client interface {
+	// Create creates a topo object
+	Create(ctx context.Context, object *topoapi.Object) error
+
+	// Update updates a topo object
+	Update(ctx context.Context, object *topoapi.Object) error
+
+	// Get gets a topo object with a given ID
+	Get(ctx context.Context, id topoapi.ID) (*topoapi.Object, error)
+
+	// Watch provides a simple facility for the application to watch for changes in the topology
+	Watch(ctx context.Context, ch chan<- topoapi.Event, opts ...WatchOption) error
+
+	// List of topo objects
+	List(ctx context.Context, opts ...ListOption) ([]topoapi.Object, error)
+}
+
+// NewClient creates a new topo client
+func NewClient(opts ...Option) (Client, error) {
+	clientOptions := Options{}
+
+	for _, opt := range opts {
+		opt.apply(&clientOptions)
 	}
 
-	// Kick off a go routine that reads events from the topology watch stream and subjects them to additional
-	// client-side filter criteria; objects that pass the filter get put on the application provided channel.
-	go handleWatchStream(ch, stream, filter...)
+	if clientOptions.Service.Host == "" || clientOptions.Service.Port == 0 {
+		clientOptions.Service.Host = DefaultServiceHost
+		clientOptions.Service.Port = DefaultServicePort
+	}
+
+	dialOpts := []grpc.DialOption{
+		grpc.WithStreamInterceptor(southbound.RetryingStreamClientInterceptor(100 * time.Millisecond)),
+		grpc.WithUnaryInterceptor(southbound.RetryingUnaryClientInterceptor()),
+	}
+	if clientOptions.Service.Insecure {
+		dialOpts = append(dialOpts, grpc.WithInsecure())
+	} else {
+		tlsConfig, err := creds.GetClientCredentials()
+		if err != nil {
+			log.Warn(err)
+			return nil, err
+		}
+
+		dialOpts = append(dialOpts, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+	}
+	conns := connection.NewManager()
+	conn, err := conns.Connect(clientOptions.Service.GetAddress(), dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	cl := topoapi.NewTopoClient(conn)
+
+	return &topo{
+		client: cl,
+	}, nil
+}
+
+// topo is the topo client
+type topo struct {
+	client topoapi.TopoClient
+}
+
+// Create creates a topo object
+func (t *topo) Create(ctx context.Context, object *topoapi.Object) error {
+	response, err := t.client.Create(ctx, &topoapi.CreateRequest{
+		Object: object,
+	})
+	if err != nil {
+		return errors.FromGRPC(err)
+	}
+	*object = *response.Object
 	return nil
 }
 
-// handleWatchStream reads events from the topology watch stream and subjects them to additional
-// client-side filter criteria; objects that pass the filter get put on the application provided channel.
-func handleWatchStream(ch chan<- topoapi.Event, stream topoapi.Topo_WatchClient, filter ...FilterOption) {
-	defer close(ch)
-	for {
-		resp, err := stream.Recv()
-		if err == io.EOF || err == context.Canceled {
-			break
-		}
-
-		if err != nil {
-			stat, ok := status.FromError(err)
-			if ok {
-				err = errors.FromStatus(stat)
-				if errors.IsCanceled(err) || errors.IsTimeout(err) {
-					break
-				}
-			}
-			log.Error("An error occurred in receiving Subscription changes", err)
-		} else if applyFilters(resp.Event, filter...) {
-			ch <- resp.Event
-		}
+// Update updates a given topo object
+func (t *topo) Update(ctx context.Context, object *topoapi.Object) error {
+	response, err := t.client.Update(ctx, &topoapi.UpdateRequest{
+		Object: object,
+	})
+	if err != nil {
+		return errors.FromGRPC(err)
 	}
+
+	*object = *response.Object
+	return nil
 }
 
-func applyFilters(event topoapi.Event, filter ...FilterOption) bool {
-	for _, f := range filter {
-		if !f.matches(event) {
-			return false
-		}
+// List lists all of topo objects
+func (t *topo) List(ctx context.Context, opts ...ListOption) ([]topoapi.Object, error) {
+	options := ListOptions{}
+
+	for _, opt := range opts {
+		opt.apply(&options)
 	}
-	return true
+
+	response, err := t.client.List(ctx, &topoapi.ListRequest{
+		Filters: options.GetFilters(),
+	})
+	if err != nil {
+		return nil, errors.FromGRPC(err)
+	}
+
+	return response.GetObjects(), nil
+}
+
+// Get get a topo object based on a given ID
+func (t *topo) Get(ctx context.Context, id topoapi.ID) (*topoapi.Object, error) {
+	response, err := t.client.Get(ctx, &topoapi.GetRequest{
+		ID: id,
+	})
+	if err != nil {
+		return nil, errors.FromGRPC(err)
+	}
+	return response.GetObject(), nil
+}
+
+// Watch watches topology events
+func (t *topo) Watch(ctx context.Context, ch chan<- topoapi.Event, opts ...WatchOption) error {
+
+	options := WatchOptions{}
+	for _, opt := range opts {
+		opt.apply(&options)
+	}
+
+	req := topoapi.WatchRequest{
+		Filters: options.GetFilters(),
+	}
+	stream, err := t.client.Watch(ctx, &req)
+	if err != nil {
+		defer close(ch)
+		return errors.FromGRPC(err)
+	}
+
+	go func() {
+		defer close(ch)
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF || err == context.Canceled {
+				break
+			}
+
+			if err != nil {
+				stat, ok := status.FromError(err)
+				if ok {
+					err = errors.FromStatus(stat)
+					if errors.IsCanceled(err) || errors.IsTimeout(err) {
+						break
+					}
+				}
+				log.Error("An error occurred in receiving topology changes", err)
+			} else {
+				ch <- resp.Event
+			}
+		}
+	}()
+	return nil
+
 }
