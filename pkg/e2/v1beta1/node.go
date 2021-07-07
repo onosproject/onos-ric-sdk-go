dd5b12a2b6323fbf02b1c994a76cc1d407500e25
adibrastegarnia <adib.rastegarnia@gmail.com>
2021-07-07 12:13:49 -0700

    revert

--- pkg/e2/v1beta1/node.go
+++ pkg/e2/v1beta1/node.go
@@ -127,8 +127,6 @@ func (n *e2Node) getRequestHeaders() e2api.RequestHeaders {
 		encoding = e2api.Encoding_PROTO
 	case ASN1Encoding:
 		encoding = e2api.Encoding_ASN1_PER
-	default:
-		encoding = e2api.Encoding(n.options.Encoding)
 	}
 	return e2api.RequestHeaders{
 		AppID:         e2api.AppID(n.options.App.AppID),
