a5815fba2739f6152de1ad77752beb0dc1eb2cdc
Thomas Vachuska <tom@opennetworking.org>
2021-10-05 20:58:52 -0700

    Added ability to start test proxy

--- pkg/utils/test_proxy.go
+++ pkg/utils/test_proxy.go
@@ -5,22 +5,43 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/onosproject/onos-lib-go/pkg/certs"
 	"github.com/onosproject/onos-proxy/pkg/manager"
+	"io/ioutil"
+	"os"
 )
 
 var mgr *manager.Manager
 
+const (
+	caCrtFile = "/tmp/onos-proxy.cacrt"
+	crtFile   = "/tmp/onos-proxy.crt"
+	keyFile   = "/tmp/onos-proxy.key"
+)
+
 // StartTestProxy starts onos-proxy instance for testing purposes
 func StartTestProxy() {
+	writeFile(caCrtFile, certs.OnfCaCrt)
+	writeFile(crtFile, certs.DefaultOnosConfigCrt)
+	writeFile(keyFile, certs.DefaultOnosConfigKey)
+
 	cfg := manager.Config{
-		CAPath:   certs.OnfCaCrt,
-		KeyPath:  certs.DefaultOnosConfigKey,
-		CertPath: certs.DefaultOnosConfigCrt,
+		CAPath:   caCrtFile,
+		KeyPath:  keyFile,
+		CertPath: crtFile,
 		GRPCPort: 5151,
 	}
 	mgr := manager.NewManager(cfg)
 	mgr.Run()
+}
+
+func writeFile(file string, s string) {
+	err := ioutil.WriteFile(file, []byte(s), 0644)
+	if err != nil {
+		fmt.Printf("error writing generated code to file: %s\n", err)
+		os.Exit(-1)
+	}
 }
 
 // StopTestProxy stops test instance of onos-proxy
