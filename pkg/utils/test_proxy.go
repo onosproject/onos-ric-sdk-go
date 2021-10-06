ae232ab89a849e850a680a4565186fbb7697b2d4
Thomas Vachuska <tom@onlab.us>
2021-10-06 13:09:07 -0700

    Adding means to start/stop onos proxy in tests (#99)
    * Adding means to start/stop onos proxy in tests
    
    * Added ability to start test proxy
    
    * Adjusting logging level for test proxy fixture

---
+++ pkg/utils/test_proxy.go
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: 2021-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+package utils
+
+import (
+	"fmt"
+	"github.com/onosproject/onos-lib-go/pkg/certs"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"github.com/onosproject/onos-proxy/pkg/manager"
+	"io/ioutil"
+	"os"
+)
+
+var mgr *manager.Manager
+
+const (
+	caCrtFile = "/tmp/onos-proxy.cacrt"
+	crtFile   = "/tmp/onos-proxy.crt"
+	keyFile   = "/tmp/onos-proxy.key"
+)
+
+// StartTestProxy starts onos-proxy instance for testing purposes
+func StartTestProxy() {
+	writeFile(caCrtFile, certs.OnfCaCrt)
+	writeFile(crtFile, certs.DefaultOnosConfigCrt)
+	writeFile(keyFile, certs.DefaultOnosConfigKey)
+
+	cfg := manager.Config{
+		CAPath:   caCrtFile,
+		KeyPath:  keyFile,
+		CertPath: crtFile,
+		GRPCPort: 5151,
+	}
+
+	logging.GetLogger("e2", "v1beta1").SetLevel(logging.WarnLevel)
+	mgr := manager.NewManager(cfg)
+	mgr.Run()
+}
+
+func writeFile(file string, s string) {
+	err := ioutil.WriteFile(file, []byte(s), 0644)
+	if err != nil {
+		fmt.Printf("error writing generated code to file: %s\n", err)
+		os.Exit(-1)
+	}
+}
+
+// StopTestProxy stops test instance of onos-proxy
+func StopTestProxy() {
+	mgr.Close()
+}
